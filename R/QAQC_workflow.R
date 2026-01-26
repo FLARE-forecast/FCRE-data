610b8af847a30997b48c3b5cac1c30c18a99a7eb
Adrienne Breef-Pilz <68744051+abreefpilz@users.noreply.github.com>
2026-01-26 14:18:55 -0500

    Updated the EDI version number

--- R/QAQC_workflow.R
+++ R/QAQC_workflow.R
@@ -18,7 +18,7 @@ setwd(home_directory)
 source('R/edi_qaqc_function.R')
 
 ## identify latest date for data on EDI (need to add one (+1) to both dates because we want to exclude all possible start_day data and include all possible data for end_day)
-package_ID <- 'edi.389.9'
+package_ID <- 'edi.389.10'
 eml <- read_metadata(package_ID)
 date_attribute <- xml_find_all(eml, xpath = ".//temporalCoverage/rangeOfDates/endDate/calendarDate")
 last_edi_date <- as.Date(xml_text(date_attribute)) + lubridate::days(1)
